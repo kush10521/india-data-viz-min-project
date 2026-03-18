5a11eea27d9c1aa539c19489371d16043c7e3330
kush10521 <kushsingh10521@gmail.com>
2026-03-19 02:47:49 +0530

    add code improvements

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ if plot:
     st.text('Size represents primary parameter')
     st.text('Color represents secondary parameter')
     if selected_state=='overall India':
-        fig = px.scatter_map(df, lat="Latitude", lon="Longitude", zoom=4,
+        fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", zoom=4,
                              size=primary,color=secondary,size_max=35,map_style="carto-positron",width=1200,height=700,hover_name='District')
         st.plotly_chart(fig)
     else:
         state_df=df[df['State']==selected_state]
-        fig = px.scatter_map(state_df, lat="Latitude", lon="Longitude", zoom=6,
+        fig = px.scatter_mapbox(state_df, lat="Latitude", lon="Longitude", zoom=6,
                              size=primary, color=secondary, size_max=25, map_style="carto-positron", width=1200,
                              height=700,hover_name='District')
         st.plotly_chart(fig)
